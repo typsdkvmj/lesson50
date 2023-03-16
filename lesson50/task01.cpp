2c28b92099f04d269884bba311c7dbfca41678db
typsdkvmj <dasha.all555@gmail.com>
2023-03-16 18:24:57 +0300

    add sum and sub function

---
+++ lesson50/task01.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int sum(int a, int b);
+int sub(int a, int b);
+
+int main() {
+
+	int a, b;
+
+	cout << "input a and b: ";
+	cin >> a >> b;
+
+	cout << "a + b = " << sum(a, b) << endl;
+	cout << "a - b = " << sub(a, b) << endl;
+
+
+	return 0;
+}
